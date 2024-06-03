71f215a2141308960be0cda3434a35f0aca5d861
1zuu <isurualagiyawanna9717@gmail.com>
2024-06-03 13:16:29 +0530

    utils

---
+++ 01-diy-llm-qa-bot/util/config.py
@@ -0,0 +1,50 @@
+import yaml, os
+
+with open("secrets.yaml", "r") as file:
+    secrets = yaml.safe_load(file)
+
+os.environ["LLAMA_CLOUD_API_KEY"] = secrets["LLAMA_CLOUD_API_KEY"]
+os.environ["VOYAGE_API_KEY"] = secrets["VOYAGE_AI_API_KEY"]
+os.environ["OPENAI_API_KEY"] = secrets["OPENAI_API_KEY"]
+os.environ["GROQ_API_KEY"] = secrets["GROQ_API_KEY"]
+
+class PATHconfig:
+    '''Configuration Paths for the project'''
+    vector_store_path = 'db/qabot_index'
+    eval_dataset_path = 'db/eval/eval_data.csv'
+    eval_results_path = 'db/eval/eval_results.csv'
+    kb_documents_dir = 'db/kb/'
+
+class DATAconfig:
+    '''Configuration class for the data'''
+    api_key = os.environ['LLAMA_CLOUD_API_KEY']
+    gpt4o_mode = False
+    chunk_size = 1000
+
+class LLMconfig:
+    '''Configuration class for the LLM model'''
+    temperature = 0.15
+    model_type = "groq"
+    model_name = {
+                "openai": "gpt-3.5-turbo",
+                "groq": "mixtral-8x7b-32768",
+                }
+    api_keys = {
+                "openai": os.environ["OPENAI_API_KEY"],
+                "groq": os.environ["GROQ_API_KEY"],
+                }
+    human_message_template = """Given the context: {context}. Answer the question {question}."""
+    system_message_template = """You are a helpful assistant built by Databricks, you are good at helping to answer a question based on the context provided, the context is a document. If the context does not provide enough relevant information to determine the answer, just say I don't know. If the context is irrelevant to the question, just say I don't know. If you did not find a good answer from the context, just say I don't know. If the query doesn't form a complete question, just say I don't know. If there is a good answer from the context, try to summarize the context to answer the question."""
+
+class EMBEDconfig:
+    '''Configuration class for the embedding model'''
+    model_type = "hf"
+    model_name = {
+                "openai": "text-embedding-3-small",
+                "voyage": "voyage-large-2-instruct",
+                "hf": "BAAI/bge-small-en-v1.5",
+                }
+    api_keys = {
+                "openai": os.environ["OPENAI_API_KEY"],
+                "voyage": os.environ["VOYAGE_API_KEY"],
+                }
---
+++ 01-diy-llm-qa-bot/util/llm_services.py
@@ -0,0 +1,69 @@
+import voyageai, tiktoken
+from llama_parse import LlamaParse
+from llama_index.core import Settings
+from llama_index.llms.groq import Groq
+from llama_index.llms.openai import OpenAI
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.embeddings.voyageai import VoyageEmbedding
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from util.config import PATHconfig, LLMconfig, EMBEDconfig, DATAconfig
+
+path_config = PATHconfig()
+embed_config = EMBEDconfig()
+data_config = DATAconfig()
+llm_config = LLMconfig()
+
+if llm_config.model_type == "openai":
+    print("Using OpenAI LLM ....")
+    llm = OpenAI(
+                api_key=llm_config.api_keys["openai"],
+                temperature=llm_config.temperature,
+                model=llm_config.model_name["openai"],
+                )
+    
+elif llm_config.model_type == "groq":
+    print("Using Groq LLM ....")
+    llm = Groq(
+                api_key=llm_config.api_keys["groq"],
+                model=llm_config.model_name["groq"],
+                )
+    
+else:
+    raise ValueError(f"Model type not supported : {llm_config.model_type}")
+
+if embed_config.model_type == "openai":
+    print("Using OpenAI Embedding ....")
+    embed_model = OpenAIEmbedding(
+                                api_key=embed_config.api_keys["openai"],
+                                model_name=embed_config.model_name["openai"]
+                                )
+    
+elif embed_config.model_type == "voyage":
+    print("Using Voyage AI Embedding ....")
+    voyageai.api_key = embed_config.api_keys["voyage"]
+    embed_model = VoyageEmbedding(
+                                api_key=embed_config.api_keys["voyage"],
+                                model_name=embed_config.model_name["voyage"]
+                                )
+    
+elif embed_config.model_type == "hf":
+    print("Using HuggingFace Embedding ....")
+    embed_model = HuggingFaceEmbedding(
+                                model_name=embed_config.model_name["hf"],
+                                # model_kwargs={"device": "mps"}
+                                )
+    
+else:
+    raise ValueError(f"Model type not supported : {embed_config.model_type}")
+
+document_parser = LlamaParse(
+                            result_type="markdown",
+                            api_key=data_config.api_key,
+                            gpt4o_mode=data_config.gpt4o_mode
+                            )
+
+tiktoken_encoder = tiktoken.get_encoding("cl100k_base")
+
+Settings.chunk_size = data_config.chunk_size
+Settings.embed_model = embed_model
+Settings.llm = llm
